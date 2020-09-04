4e4f95410b5d7009b51b11e1c25d45e5249f4bea
Kuswandi <wandinak17@gmail.com>
2020-09-04 22:20:49 +0700

    au

--- src/components/school/Profile.vue
+++ src/components/school/Profile.vue
@@ -5,7 +5,7 @@
 				<div class="h-100">
 					<div class="d-flex flex-column flex-center">
 						<div class="bgi-no-repeat bgi-size-cover rounded min-h-180px w-100" style="background-image: url(/img/img-70.jpg)"></div>
-						<span class="card-title font-weight-bolder text-dark-75 text-hover-primary font-size-h4 m-0 pb-1">{{ school.settings.name }}</span>
+						<span class="card-title font-weight-bolder pt-7 text-dark-75 text-hover-primary font-size-h4 m-0 pb-1">{{ school.settings.name }}</span>
 						<div class="font-weight-bold text-dark-50 font-size-sm pb-7">{{ school.settings.email }}</div>
 					</div>
 					<div class="pt-1 text-center">
